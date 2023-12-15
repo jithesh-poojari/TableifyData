ee3f665de16c4de786953173aade9c4758971b52
Jithesh <96974839+Jithesh-Poojari@users.noreply.github.com>
2023-12-15 17:12:25 +0530

    input option styled

--- components/input/ApiInput.jsx
+++ components/input/ApiInput.jsx
@@ -25,21 +25,23 @@ const ApiInput = ({ onSubmit }) => {
 
   return (
     <form className="flex flex-col items-center justify-between p-2 bg-white border rounded-md shadow-md md:p-4 md:gap-2 dark:bg-gray-800 md:flex-row dark:border-gray-600 dark:shadow-gray-700">
-      <div className="relative mb-2 md:max-w-min md:text-base md:mb-0 dark:bg-gray-700 dark:border-gray-500 dark:text-white md:w-fit md:flex-grow ">
-        <div className="inline w-full p-2 bg-white rounded-md cursor-pointer peer whitespace-nowrap">
-          {apiType.toUpperCase()}
+      <div className="relative w-full mb-2 text-sm font-semibold md: md:max-w-min md:text-base md:mb-0 dark:bg-gray-700 dark:border-gray-500 dark:text-white md:w-fit md:flex-grow ">
+        <div className="w-full p-2 text-center bg-white border-2 rounded-md cursor-pointer peer whitespace-nowrap">
+          {apiType.toUpperCase()} 
         </div>
-        <div className="absolute right-0 hidden mt-2 bg-white rounded-md shadow-md peer-hover:block">
-          <div className="p-2 text-gray-800 cursor-pointer dark:text-white hover:bg-gray-100 hover:text-gray-900 whitespace-nowrap">
+       <div className="absolute left-0 hidden peer-hover:block hover:block">
+       <div className="mt-2 bg-white divide-y-2 rounded-md shadow-md ">
+          <div className="px-4 py-2 text-gray-800 cursor-pointer dark:text-white hover:bg-gray-100 hover:text-gray-900 whitespace-nowrap">
             API URL
           </div>
-          <div className="p-2 text-gray-800 cursor-pointer dark:text-white hover:bg-gray-100 hover:text-gray-900 whitespace-nowrap">
+          <div className="px-4 py-2 text-gray-800 cursor-pointer dark:text-white hover:bg-gray-100 hover:text-gray-900 whitespace-nowrap">
             JSON Text
           </div>
-          <div className="p-2 text-gray-800 cursor-pointer dark:text-white hover:bg-gray-100 hover:text-gray-900 whitespace-nowrap">
+          <div className="px-4 py-2 text-gray-800 cursor-pointer dark:text-white hover:bg-gray-100 hover:text-gray-900 whitespace-nowrap">
             JSON File
           </div>
         </div>
+       </div>
       </div>
 
       {apiType === "api url" && (
